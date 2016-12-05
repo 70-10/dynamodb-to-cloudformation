54292e75630adff0fbb2a6fc11d642589657d009
70_10 <7010.dev@gmail.com>
2016-12-05 14:31:23 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# DynamoDB To CloudFormation
+
+## Install
+
+```
+$ npm install -g 70-10/dynamodb-to-cloudformation
+```
+
+## Usage
+
+```
+$ dtc <TableName>
+```
---
+++ index.js
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+
+const AWS = require("./aws");
+const co = require("co");
+const clone = require("lodash.cloneDeep");
+const pkg = require("./package.json");
+const program = require("commander");
+
+if (process.argv.length < 3) {
+  process.argv.push("--help");
+}
+
+program.version(pkg.version).parse(process.argv);
+
+if(program.args.length !== 1) {
+  console.error("table name is undefined");
+  return;
+}
+
+co(get(program.args[0])).catch(e => console.error(e.message));
+
+function *get(tableName) {
+  const DynamoDB = new AWS.DynamoDB();
+
+  const {Table} = yield DynamoDB.describeTable({
+    TableName: tableName,
+  }).promise();
+
+  const result = shaping(Table);
+  console.log(JSON.stringify(result, null, 2));
+}
+
+function shaping(table) {
+  let result = clone(table);
+
+  if(result.GlobalSecondaryIndexes && result.GlobalSecondaryIndexes.length >= 1) {
+    result.GlobalSecondaryIndexes = result.GlobalSecondaryIndexes.map(e => {
+      delete e.IndexArn;
+      delete e.ItemCount;
+      delete e.IndexStatus;
+      delete e.IndexSizeBytes;
+      delete e.ProvisionedThroughput.NumberOfDecreasesToday;
+      delete e.ProvisionedThroughput.LastIncreaseDateTime;
+      delete e.ProvisionedThroughput.LastDecreaseDateTime;
+      return e;
+    });
+  }
+
+  if(result.LocalSecondaryIndexes && result.LocalSecondaryIndexes.length >= 1) {
+    result.LocalSecondaryIndexes = result.LocalSecondaryIndexes.map(e => {
+      delete e.IndexArn;
+      delete e.IndexStatus;
+      delete e.ItemCount;
+      delete e.IndexSizeBytes;
+      delete e.ProvisionedThroughput.NumberOfDecreasesToday;
+      delete e.ProvisionedThroughput.LastIncreaseDateTime;
+      delete e.ProvisionedThroughput.LastDecreaseDateTime;
+      return e;
+    });
+  }
+  delete result.ProvisionedThroughput.NumberOfDecreasesToday;
+  delete result.ProvisionedThroughput.LastIncreaseDateTime;
+  delete result.ProvisionedThroughput.LastDecreaseDateTime;
+  delete result.TableArn;
+  delete result.TableName;
+  delete result.TableSizeBytes;
+  delete result.TableStatus;
+  delete result.ItemCount;
+  delete result.CreationDateTime;
+  return result;
+}
